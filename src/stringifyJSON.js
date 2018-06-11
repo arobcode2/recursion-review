34b504740a675d60457c5d0858ece7f080d5fb05
arobcode2 <andrew-c-rob@live.com>
2018-06-11 16:07:58 -0500

    Implement stringifyJSON()

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,50 @@
 
 // but you don't so you're going to write it from scratch:
 
+
+//takes an object and makes it into a JSON string
+//what kind of object is being passed in?
+//base case: if the object is not an array or an object return the stringified input
+//recursive case: if the object is an array- the first thing to add is the opening bracket- iterates through the array calling stringify on each element and seperating with a comma, after done iterating concatenate with a closing bracket
+//object case: open curly brace, iterate through keys, stringify the value of each key, concatenate with colons and commas, closing curly brace
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var result = '';
+  var count = 0;
+
+  if (Array.isArray(obj)) {
+    result += '[';
+    for (var i = 0; i < obj.length; i++) {
+      result += stringifyJSON(obj[i]);
+      if (i !== obj.length - 1) {
+        result += ',';    
+      }
+    }
+    result += ']';
+  } else if (typeof obj === 'number') {
+    result += obj.toString();
+  } else if (obj === null) {
+    result += 'null';
+  } else if (typeof obj === 'boolean') {
+    result += obj.toString();
+  } else if (typeof obj === 'string') {
+    result += '"' + obj + '"';
+  } else if (typeof obj === 'object') {
+    result += '{';
+    for (var key in obj) {
+      if (obj[key] === undefined || typeof obj[key] === 'function') {
+        continue;
+      }
+      result += '"' + key + '":' + stringifyJSON(obj[key]);
+      count++;
+      if (count < Object.keys(obj).length) {
+        result += ',';
+      }
+    }
+    result += '}';
+  }
+    
+  
+
+  return result;
+
 };
